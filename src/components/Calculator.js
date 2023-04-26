667f242021f09c76122c6d81035282d0919a289b
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-04-26 11:34:05 +0500

    Add calculator UI components

---
+++ src/components/Calculator.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CalculatorBtns from './CalculatorBtns';
+
+export default function Calculator() {
+  return (
+    <div className="calculatorBody">
+      <CalculatorBtns />
+    </div>
+  );
+}
